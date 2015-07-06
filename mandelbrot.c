c4f6fbdeb48649a048678a62f4284446cdb6ec5f
STeofilo <samuel.teofilo@hotmail.com>
2015-07-06 20:58:55 -0300

    Madelbrot Original
    Alterado variaveis double para float

--- mandelbrot.c
+++ mandelbrot.c
@@ -26,14 +26,14 @@ int main()
         const int iXmax = 16384; 
         const int iYmax = 16384;
         /* world ( double) coordinate = parameter plane*/
-        double Cx,Cy;
-        const double CxMin=-2.5;
-        const double CxMax=1.5;
-        const double CyMin=-2.0;
-        const double CyMax=2.0;
+        float Cx,Cy;
+        const float CxMin=-2.5;
+        const float CxMax=1.5;
+        const float CyMin=-2.0;
+        const float CyMax=2.0;
         /* */
-        double PixelWidth=(CxMax-CxMin)/iXmax;
-        double PixelHeight=(CyMax-CyMin)/iYmax;
+        float PixelWidth=(CxMax-CxMin)/iXmax;
+        float PixelHeight=(CyMax-CyMin)/iYmax;
         /* color component ( R or G or B) is coded from 0 to 255 */
         /* it is 24 bit color RGB file */
         const int MaxColorComponentValue=255; 
@@ -41,14 +41,14 @@ int main()
         char *filename="mandelbrot.ppm";
         static unsigned char color[3];
         /* Z=Zx+Zy*i  ;   Z0 = 0 */
-        double Zx, Zy;
-        double Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
+        float Zx, Zy;
+        float Zx2, Zy2; /* Zx2=Zx*Zx;  Zy2=Zy*Zy  */
         /*  */
         int Iteration;
         const int IterationMax=256;
         /* bail-out value , radius of circle ;  */
-        const double EscapeRadius=2;
-        double ER2=EscapeRadius*EscapeRadius;
+        const float EscapeRadius=2;
+        float ER2=EscapeRadius*EscapeRadius;
         /*create new file,give it a name and open it in binary mode  */
         fp= fopen(filename,"wb"); /* b -  binary mode */
         /*write ASCII header to the file*/
@@ -72,22 +72,22 @@ int main()
                         for (Iteration=0;Iteration<IterationMax && ((Zx2+Zy2)<ER2);Iteration++)
                         {
                             Zy=2*Zx*Zy + Cy;
-                            Zx=Zx2-Zy2 +Cx;
+                            Zx=Zx2-Zy2 + Cx;
                             Zx2=Zx*Zx;
                             Zy2=Zy*Zy;
                         };
                         /* compute  pixel color (24 bit = 3 bytes) */
                         if (Iteration==IterationMax)
                         { /*  interior of Mandelbrot set = black */
-                           color[0]=0;
-                           color[1]=0;
-                           color[2]=0;                           
+							color[0]=0;
+							color[1]=0;
+							color[2]=0;                           
                         }
                         else 
                         { /* exterior of Mandelbrot set = white */
-                             color[0]=((IterationMax-Iteration) % 8) *  63;  /* Red */
-                             color[1]=((IterationMax-Iteration) % 4) * 127;  /* Green */ 
-                             color[2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
+							color[0]=((IterationMax-Iteration) % 8) *  63;  /* Red */
+                            color[1]=((IterationMax-Iteration) % 4) * 127;  /* Green */ 
+                            color[2]=((IterationMax-Iteration) % 2) * 255;  /* Blue */
                         };
                         /*write color to the file*/
                         fwrite(color,1,3,fp);
@@ -101,3 +101,12 @@ int main()
 		system("PAUSE");
         return 0;
 }
+
+	/*
+	unsigned char *colorR = (unsigned char *) malloc(iXmax*iYmax);
+	unsigned char *colorG = (unsigned char *) malloc(iXmax*iYmax);
+	unsigned char *colorB = (unsigned char *) malloc(iXmax*iYmax);
+
+	unsigned char *R = colorR;
+	unsigned char *G = colorG;
+	unsigned char *B = colorB;*/
